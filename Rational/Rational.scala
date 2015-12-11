af6517a8fb9f263e29e9e1ac998d26f2576d84be
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-12-11 15:24:43 -0800

    Added gcd function calls and ability to use lowest terms for rationals

--- Rational/Rational.scala
+++ Rational/Rational.scala
@@ -2,9 +2,12 @@
 class Rational(n: Int, d: Int)
 {
 	require(d != 0)
-	val numer: Int = n
-	val denom: Int = d
+	private val g = gcd(n.abs, d.abs)
+	val numer = n / g
+	val denom = d / g
 	def this(n: Int) = this(n, 1)	// auxillary constructor
-	override def toString = numer + "/" + denom
 	def add(that: Rational): Rational = new Rational(numer * that.denom + that.numer * denom, denom * that.denom)
+	override def toString = numer + "/" + denom
+	
+	private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
