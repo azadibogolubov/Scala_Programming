387d0780f888ffa35d80420c8064009a025abb8a
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-12-12 11:59:20 -0800

    More file manipulation games

--- FileManip/FileListing.scala
+++ FileManip/FileListing.scala
@@ -1,10 +1,22 @@
+// Based on chapter 7 of Programming in Scala 2nd Edition
 object FileListing
 {
 	def main(args: Array[String])
 	{
 		val filesHere = (new java.io.File(".")).listFiles
-
+		var i = 1
+		println("Listing contents of current directory")
 		for (file <- filesHere)
+		{
+			println(i + ". " + file)
+			i = i + 1
+		}
+		
+		println("")
+		println("Now listing all .scala files")
+		for (file <- filesHere
+		 	if file.isFile 
+			if file.getName.endsWith(".scala"))
 			println(file)
 	}
 }
