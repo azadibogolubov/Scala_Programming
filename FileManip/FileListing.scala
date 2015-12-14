5a827a8cb6664367fa38936d63dcfeb7193cd22d
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-12-13 19:33:23 -0800

    Added grep

--- FileManip/FileListing.scala
+++ FileManip/FileListing.scala
@@ -4,6 +4,7 @@ object FileListing
 	def main(args: Array[String])
 	{
 		val filesHere = (new java.io.File(".")).listFiles
+		val pattern = args(0)
 		var i = 1
 		println("Listing contents of current directory")
 		for (file <- filesHere)
@@ -18,5 +19,17 @@ object FileListing
 		 	if file.isFile 
 			if file.getName.endsWith(".scala"))
 			println(file)
+
+		for 
+                {
+                        file <- filesHere
+                        if file.getName.endsWith(".scala")
+                        line <- fileLines(file)
+                        trimmed = line.trim
+                        if trimmed.matches(pattern)
+                }
+                println(file + ": " + trimmed)
 	}
+	
+	def fileLines(file: java.io.File) = scala.io.Source.fromFile(file).getLines().toList
 }
